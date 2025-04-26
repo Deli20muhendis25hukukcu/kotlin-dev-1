cb03a457dd4a96cd8ecac1c2f3361180c43798d9
rabiasandal <rabia.sandal34@gmail.com>
2025-04-26 18:43:06 +0300

    Add files via upload
    kotlin 1.ödevim

---
+++ kotlin ödev 1.kt
@@ -0,0 +1,76 @@
+
+fun main() {
+    val ad = "Begonya"
+    val sehir = "Gaziantep"
+    val ulke = "Türkiye"
+    val telefon = "+905432623902"
+    val postaKodu = "27000"
+    val email = "begonya@example.com"
+    val meslek = "Hukuk Öğrencisi"
+    val stokMiktar = 15
+    val musteriAdi = "Simay"
+    val bakiye = 2500.75
+    val dogumGunu = "27 Temmuz"
+    val maas = 12000
+    val medeniDurum = "Bekar"
+    val urunYorum = "Çok kaliteli"
+    val odemeTarih = "2025-04-30"
+    val odeme = 899.99
+    val siparisAdet = 3
+    val arabaModel = "Fiat Egea"
+    val kitapAdi = "Suç ve Ceza"
+    val yayinTarihi = "1866"
+    val indirimMiktari = "20%"
+    val odaSayisi = 2
+    val enlem = 37.0662
+    val boylam = 37.3833
+    val urunAdi = "Kulaklık"
+    val yemekFiyat = 89.99
+    val marka = "Sony"
+    val muzikAdi = "Clair de Lune"
+    
+    val sokakAdi = "Gül Sokak"
+    val otobusHatti = "79A"
+    val kalanDakika = 12
+    val takipKodu = "TR123456789"
+    val kuponSure = "7 gün"
+    val kuponKodu = "INDIRIM20"
+    val faturaAdresi = "Gaziantep, Şahinbey, Gül Sokak No:10"
+
+    println("---------- KİŞİSEL BİLGİLER ----------")
+    println("Merhaba, ben $ad!")
+    println("Yaşadığım şehir: $sehir")
+    println("Ülkem: $ulke")
+    println("İletişim numaram: $telefon")
+    println("Posta kodum: $postaKodu")
+    println("E-postam: $email")
+    println("Mesleğim: $meslek")
+    println("Stok miktarım: $stokMiktar")
+    println("Müşteri adım: $musteriAdi")
+    println("Bakiyem: $bakiye")
+    println("Doğum günüm: $dogumGunu")
+    println("Maaşım: $maas TL")
+    println("Medeni durumum: $medeniDurum")
+    println("Ürün yorumum: $urunYorum")
+    println("Ödeme tarihim: $odemeTarih")
+    println("Ödemem: $odeme TL")
+    println("Sipariş adetim: $siparisAdet")
+    println("Araba modelim: $arabaModel")
+    println("Kitap adım: $kitapAdi")
+    println("Yayınlama tarihim: $yayinTarihi")
+    println("İndirim miktarım: $indirimMiktari")
+    println("Oda sayım: $odaSayisi")
+    println("Enlemim: $enlem")
+    println("Boylamım: $boylam")
+    println("Ürün adım: $urunAdi")
+    println("Yemek fiyatım: $yemekFiyat TL")
+    println("Markam: $marka")
+    println("Müzik adım: $muzikAdi")
+    println("Sokak adım: $sokakAdi")
+    println("Otobüs hattım: $otobusHatti")
+    println("Kalan dakikam: $kalanDakika")
+    println("Takip kodum: $takipKodu")
+    println("Kupon sürem: $kuponSure")
+    println("Kupon kodum: $kuponKodu")
+    println("Fatura adresim: $faturaAdresi")
+}
